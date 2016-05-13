cdd89b1a52cb271e873de8c4f663747fc258bf4b
mchandu123 <mchandu123@gmail.com>
2016-05-13 12:01:20 +0530

    Rename Drop_Tasko.sql to SqlScripts/Drop_Tasko.sql

---
+++ SqlScripts/Drop_Tasko.sql
@@ -0,0 +1,28 @@
+﻿/* This sql file is used to drop all the used Tables and stored procedures */
+Use [Tasko]
+Go
+
+Drop Table [dbo].[Issues]
+Go
+Drop Table [dbo].[IdProofs]
+Go
+Drop Table [dbo].[Order]
+Go
+Drop Table [dbo].[Vendor]
+Go
+Drop Table [dbo].[Services]
+Go
+Drop Table [dbo].[OrderStatus]
+Go
+Drop Table [dbo].[Customer]
+Go
+Drop Table [dbo].[VendorServices]
+Go
+Drop Table [dbo].[VendorProof]
+Go
+Drop Table [dbo].[VendorIssues]
+Go
+Drop Table [dbo].[VendorRating]
+Go
+Drop Table [dbo].[CustomerRating]
+Go
